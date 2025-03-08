fffab7723a7ab44f96aaa04e4fb046cfe5ec3b91
DevTamakuwala <dev.official2424@gmail.com>
2025-03-09 00:55:18 +0530

    Security Configured

---
+++ whatsappclone/src/test/java/io/github/devtamakuwala/whatsapp_clone/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devtamakuwala.whatsapp_clone.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
